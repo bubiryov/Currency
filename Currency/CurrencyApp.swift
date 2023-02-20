30d7a77945aa172803bd8430d9e1db4391304641
Egor <bubiryov@icloud.com>
2023-02-20 18:58:33 +0200

    Initial Commit

---
+++ Currency/CurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyApp.swift
+//  Currency
+//
+//  Created by Egor Bubiryov on 20.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
